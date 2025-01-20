c06c354f3e284eda1f915214cf02ab0fd05f4064
Torben Haenke <torben.haenke@web.de>
2025-01-20 14:21:30 +0000

    added split and normalize scripts to /src/data and the output files to data/processed

---
+++ src/data/normalize.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+X_train = pd.read_csv('data/processed_data/X_train.csv', index_col = 0)
+X_test = pd.read_csv('data/processed_data/X_test.csv', index_col = 0)
+
+# I decided to go with the standart scaler, from quickly looking at the data
+# I think most of the variables are normal distributed, but more preprocessing would be need
+scaler = StandardScaler()
+
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+X_train_scaled = pd.DataFrame(X_train_scaled, columns = X_train.columns)
+X_test_scaled = pd.DataFrame(X_test_scaled, columns = X_test.columns)
+
+# reset the index, in case it might be needed later, since it got lost during scaling
+X_train_scaled.index = X_train.index
+X_test_scaled.index = X_test.index
+
+X_train_scaled.to_csv('data/processed_data/X_train_scaled.csv')
+X_test_scaled.to_csv('data/processed_data/X_test_scaled.csv')
--- src/data/train_test_split.py
+++ src/data/train_test_split.py
@@ -2,12 +2,16 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('data/raw_data/raw.csv')
-
 # the date is not a describing variable and silica_concentrate is the target variable
-X = df.drop(columns = ['date', 'silica_concentrate'])
-y = df['silica_concentrate']
+df = df.drop(columns = ['date'])
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 1403)
+df_train, df_test = train_test_split(df, test_size = 0.25, random_state = 1403)
+
+X_train = df_train.drop(columns = ['silica_concentrate'])
+y_train = df_train['silica_concentrate']
+
+X_test = df_test.drop(columns = ['silica_concentrate'])
+y_test = df_test['silica_concentrate']
 
 X_train.to_csv('data/processed_data/X_train.csv')
 X_test.to_csv('data/processed_data/X_test.csv')
